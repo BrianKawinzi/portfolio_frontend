1f8ffd03837564ca06e245a9f429f9caa46f5d05
BrianKawinzi <brian.mutisya@strathmore.edu>
2023-09-17 06:31:33 +0300

    hamburger menu setup

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    //hamburger Toogle
+    const hamburger = document.querySelector('.hamburger');
+    const menuList = document.querySelector('.menu-list');
+    const windowWidth = window.innerWidth;
+
+    if (hamburger && menuList) {
+      hamburger.addEventListener('click', (event) => {
+        menuList.classList.toggle('active');
+        event.preventDefault();
+      });
+
+      const menuLinks = document.querySelectorAll('.menu-list li a');
+      menuLinks.forEach((link) => {
+        link.addEventListener('click', (event) => {
+          if (windowWidth < 768) {
+            menuList.classList.remove('active');
+            event.preventDefault();
+          }
+        });
+      });
+    }
+  }
 
 }
